ddad85d5b08ffe693b55c896f8dc20af78a7843c
Bornfromashes <amit220698@gmail.com>
2017-04-10 23:36:40 +0530

    Updating models

--- mail/ammy/models.py
+++ mail/ammy/models.py
@@ -6,14 +6,13 @@ from django.db import models
 class student_info(models.Model):
     name=models.CharField(max_length=40, help_text="Enter Name")
     reg_no=models.IntegerField(help_text='Enter your reg_no', primary_key=True)
-    reg_no=models.ForeignKey('reg_no', on_delete=models.CASCADE)
     email=models.EmailField(help_text='Enter email')
     def __str__(self):
         return self.name
 
 class History(models.Model):
-    reg_no=models.ForeignKey('reg_no', on_delete=models.CASCADE)
-    reg_no=models.IntegerField(primary_key=True)
+
+    Reg_no=models.ForeignKey('student_info', on_delete=models.CASCADE)
     date=models.DateTimeField(auto_now=True)
 
 class Admin(models.Model):
--- mail/ammy/views.py
+++ mail/ammy/views.py
@@ -1,3 +1,18 @@
+from django.http import HttpResponse
+from django.template import loader
+from django.http import Http404
 from django.shortcuts import render
+from .models import student_info
+from django.shortcuts import render
+from PIL import Image
+import tesserocr
+
+
+
 
 # Create your views here.
+def index(request):
+    im= input('Enter image here')
+    image=Image.open(im)
+    t= tesserocr.image_to_text(image)
+    print t
